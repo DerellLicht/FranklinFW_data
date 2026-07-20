a4db682beb407b2cb22947ad75b9bfbe633ab64e
Derell Licht <derelict@comcast.net>
2026-07-20 10:09:41 -0700

    update scripts and data files

--- get_week_data.cmd
+++ get_week_data.cmd
@@ -1,4 +1,11 @@
 set ICLOUD_DRV="D:\images\iCloud Photos\iCloudDrive\Downloads\Images"
 
+@if /I "%~1"=="list" goto :list
+
 copy %ICLOUD_DRV%\data*week.csv data
 del %ICLOUD_DRV%\data*week.csv
+   @goto :eof
+
+:list
+c:\utility\ndir64 %ICLOUD_DRV% -1
+   @goto :eof
